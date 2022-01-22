ec82f9de7b053debebd3d04a1e88419f9c9c8528
slm17 <slm17slm17@gmail.com>
2022-01-22 02:33:40 -0300

    added pagination

--- js/main.js
+++ js/main.js
@@ -1,17 +1,47 @@
-console.log('Test js')
+console.log('Test 00')
 
 const _pokemonContainer = document.querySelector('.pokemon-container');
-// const _spinner = document.querySelector('.spinner-border'); 
+const _spinner = document.querySelector('.spinner-border'); 
+
+const _previousPagination = document.querySelector('#previous-pagination');
+const _nextPagination = document.querySelector('#next-pagination');
+
+let offset = 1 ;
+let limit = 8 ;
+
+_previousPagination.addEventListener('click', () =>{
+    if(offset != 1){
+        offset -= 9;
+        removeChildNodes(_pokemonContainer);
+    fetchPokemons(offset,limit)
+    }
+})
+
+_nextPagination.addEventListener('click', () =>{
+    offset += 9;
+    // _pokemonContainer.innerHTML = fetchPokemons(offset,limit)
+    removeChildNodes(_pokemonContainer);
+    fetchPokemons(offset,limit)
+
+})
 
 // funcion para llamar 1 pokemon
 function fetchPokemon(id){
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
     .then( res => res.json())
-    .then(data => {createPokemon(data)})    
+    .then(data => {createPokemon(data)
+    _spinner.style.display = 'none';
+    _previousPagination.style.display = 'block';
+    _nextPagination.style.display = 'block';
+    });    
 }
 // funcion para llamar varios pokemones
-function fetchPokemons (number){
-    for(let i = 1; i <= number; i++){
+function fetchPokemons (offset,limit){
+    _spinner.style.display = 'block';
+    _previousPagination.style.display = 'none';
+    _nextPagination.style.display = 'none';
+
+    for(let i = offset; i <=  offset + limit; i++){
         fetchPokemon(i);
         // console.log(fetchPokemon(i))
     }
@@ -43,6 +73,13 @@ function createPokemon(pokemon){
 
     _pokemonContainer.appendChild(card);    
 }
-
+// funcion para eliminar tarjetas
+function removeChildNodes(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild)
+    }
+}
 // fetchPokemon(1);
-fetchPokemons(9);
+fetchPokemons(offset,limit);
+
+console.log('Test 01 OK')
